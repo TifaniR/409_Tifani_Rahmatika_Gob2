0d4c0e6548488911f60a237f32094dea7dded19c
TifaniR <fanirahmatika0305@gmail.com>
2022-09-22 21:53:32 +0700

    update

---
+++ Sesi-01/variabel-&-tipedata/variabel.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = Tifani
+	var age int = 25
+
+	fmt.Println("ini adalah namanya ==>", name)
+	fmt.println("ini adalah umurnya ==>", age)
+}
